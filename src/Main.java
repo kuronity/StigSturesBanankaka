90998906e1fbf053c391401294e4cc087963a843
kuronity <kurobjorn@gmail.com>
2023-01-23 16:11:40 +0100

    added new feature sucks

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hej 445");
         System.out.println("BBBBAAADDDDDIIIIEEEESSS");
-        System.out.println("SYKE");
+        System.out.println("NEW FEATURE");
+        System.out.println("The new feature sucks.");
 
     }
 }
